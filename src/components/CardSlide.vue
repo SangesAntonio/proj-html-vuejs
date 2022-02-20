e5112b2b2ae09df2defc8f9a9dff50b8e390bddb
SangesAntonio <sanges65@gmail.com>
2022-02-20 11:13:25 +0100

    added cards slider testimonials

---
+++ src/components/CardSlide.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white">
+    <h5 class="fw-bold mb-3">{{ title }}</h5>
+    <p class="lh-lg text-secondary">{{ paragraph }}</p>
+    <div class="card mb-3 border-0" style="max-width: 540px">
+      <div class="row g-0 align-items-center">
+        <div class="col-md-3">
+          <img
+            :src="require(`../assets/img/${imag}`)"
+            class="img-fluid rounded-cs"
+            alt="..."
+          />
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">{{ name }}</h5>
+
+            <p class="card-text">
+              <small class="">/{{ role }}</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardSlide",
+  props: ["title", "paragraph", "imag", "name", "role"],
+};
+</script>
+
+<style scoped lang='scss'>
+.rounded-cs {
+  border-radius: 50%;
+}
+</style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,18 +1,80 @@
-<template>
-  <div>
-    <TitleSections titleOne="Testimonials" />
+<template >
+  <div class="container-cs">
+    <div class="row justify-content-center bg-color">
+      <div class="col-12">
+        <div class="row justify-content-center">
+          <TitleSections
+            titleOne="Testimonials"
+            titleTwo="Why do people love me"
+            className="fw-normal"
+          />
+        </div>
+      </div>
+      <div class="row mt-4 justify-content-between p-4">
+        <CardSlide
+          class="col-3"
+          v-for="(card, index) in testimonials"
+          :key="index"
+          :title="card.titleOne"
+          :paragraph="card.parag"
+          :imag="card.imag"
+          :name="card.name"
+          :role="card.role"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import TitleSections from "./TitleSections.vue";
+import CardSlide from "./CardSlide.vue";
 export default {
   name: "Slide",
   components: {
     TitleSections,
+    CardSlide,
+  },
+  data() {
+    return {
+      testimonials: [
+        {
+          titleOne: "High level of efficiency and scientific teaching methods",
+          parag:
+            "i am free to learn at my own pace, follow my own schedule and choose the subjectI want to learn from the syllabus. Great study portal for people like me.",
+          imag: "artist-testimonial-avatar-04.jpg",
+          name: "Mina Hallace",
+          role: "freelancer",
+        },
+        {
+          titleOne:
+            "Professional team of specialists and passionate mentors at reach",
+          parag:
+            "I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.",
+          imag: "artist-testimonial-avatar-03.jpg",
+          name: "Madlei Pondor",
+          role: "IT specialist",
+        },
+        {
+          titleOne: "It's a choice of quality for people with special needs",
+          parag:
+            "i ma very strict person so I require everything to be organized and neat. Then, I'll be able to make things right and shine. MaxCoach guys just got me.",
+          imag: "artist-testimonial-avatar-02.jpg",
+          name: "Florence Themes",
+          role: "Multimedia Admin",
+        },
+      ],
+    };
   },
 };
 </script>
 
-<style>
+<style scoped lang='scss'>
+.container-cs {
+  width: 90%;
+  margin: 0 auto;
+}
+.bg-color {
+  background-color: #faf8f6;
+}
 </style>
